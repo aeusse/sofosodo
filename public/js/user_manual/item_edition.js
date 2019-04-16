1e0f850199a37762c8d159308c96f820e6f5c3db
aeusse <aeusse@grupodyd.com>
2019-04-16 15:40:34 -0500

    Firebase storage. Edición de ítem.

--- public/js/user_manual/item_edition.js
+++ public/js/user_manual/item_edition.js
@@ -1,13 +1,117 @@
+const storageService = firebase.storage();
+const storageRef = storageService.ref();
 
+function editItem(path) {
+    //console.log(path)
 
-function editItem(path, title){
-    console.log(path)
-    console.log(title)
+    let o = tree;
+    for (let i = 0; i < path.length - 1; i++) {
+        let n = path[i];
+        o = o[n].children;
+    }
+    const closerParentIdx = path[path.length - 1];
+    const targetObject = o[closerParentIdx];
+
     $("#tree_container").hide();
     $("#item_edition_title").show();
     $("#item_edition_body").show();
 
-    $("#item_edition_title").html(title);
+    $("#item_edition_title").html(targetObject.title);
+
+    console.log(targetObject.data)
 }
 
-editItem([1, 0, 1], "El título _1 0 1")
+//editItem([1, 0, 1], "El título _1 0 1");
+
+
+
+function handleFileUploadSubmit(e) {
+    const targetIdx = $(e.target).data().idx;
+    const selectedFile = $(`#file-select_${targetIdx}`)[0].files[0]
+    const uploadRef = storageRef.child(`images/${selectedFile.name}`);
+    const uploadTask = uploadRef.put(selectedFile); //create a child directory called images, and place the file inside this directory
+    uploadTask.on('state_changed', (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+    }, (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+    }, async () => {
+        // Do something once upload is complete
+        console.log('success');
+        const uploadedImageUrl = await uploadRef.getDownloadURL();
+        $(`#content_${targetIdx} div.uploadedImageDivs`).empty();
+        $(`#content_${targetIdx} div.uploadedImageDivs`).prepend(`<img src="${uploadedImageUrl}" />`);
+        $(`#content_${targetIdx} div.uploadedImageDivs`).data("img_url", uploadedImageUrl);
+    });
+}
+
+function addParagraph(idx) {
+    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final"><label class="where_to_show_checks"> Usuario final</label><br>
+      <textarea id="textarea_${idx}"></textarea>
+      <br>
+      <br>
+      <button class="add_text_or_image_buttons" onclick="addParagraph(${idx + 1});">Texto</button>
+      <button class="add_text_or_image_buttons" onclick="addImage(${idx + 1});">Imagen</button>`;
+    if ($(`#content_${idx}`).length === 0) {
+        const html = `<div id="content_${idx}" class="contents" data-idx="${idx}" data-type="text">` + innerHtml + `</div>`;
+        $("#developer_edition_zone").append(html);
+    } else {
+        $(`#content_${idx}`).empty();
+        $(`#content_${idx}`).html(innerHtml);
+        $(`#content_${idx}`).data("type", "text");
+    }
+}
+function addImage(idx) {
+    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final"><label class="where_to_show_checks"> Usuario final</label><br>
+      <div class="uploadedImageDivs"></div>
+      <input id="file-select_${idx}" type="file" class="add_text_or_image_buttons" accept="image/*" />
+      <button id="file-submit_${idx}" data-idx="${idx}" class="add_text_or_image_buttons">Subir</button>
+      <br>
+      <br>
+      <button class="add_text_or_image_buttons" onclick="addParagraph(${idx + 1});">Texto</button>
+      <button class="add_text_or_image_buttons" onclick="addImage(${idx + 1});">Imagen</button>`;
+    if ($(`#content_${idx}`).length === 0) {
+        const html = `<div id="content_${idx}" class="contents" data-idx="${idx}" data-type="image">` + innerHtml + `</div>`;
+        $("#developer_edition_zone").append(html);
+    } else {
+        $(`#content_${idx}`).empty();
+        $(`#content_${idx}`).html(innerHtml);
+        $(`#content_${idx}`).data("type", "image");
+    }
+    $(document).on('click', `#file-submit_${idx}`, handleFileUploadSubmit);
+}
+
+function saveToLocalStorage(path){
+    const elements = $(".contents");
+    const data = {}
+    for (let i of elements){
+        const dataIdx = $(i).data().idx;
+        data[dataIdx] = {type: $(i).data().type}
+        if (data[dataIdx].type === "image"){
+            data[dataIdx].url = $(`#content_${dataIdx} div.uploadedImageDivs`).data().img_url
+            if (data[dataIdx].url === undefined){
+                data[dataIdx].url = "";
+            }
+        }else{
+            data[dataIdx].text = $(`#textarea_${dataIdx}`).val();
+        }
+        if($(`#where_to_show_checkbox_${dataIdx}`).is(':checked')) {
+            data[dataIdx].finalUser = true;
+        }else{
+            data[dataIdx].finalUser = false;
+        }
+    }
+
+    let o = tree;
+    for (let i = 0; i < path.length - 1; i++) {
+        let n = path[i];
+        o = o[n].children;
+    }
+    const closerParentIdx = path[path.length - 1];
+    const target = o[closerParentIdx];
+
+    target.data = data;
+    localStorage.setItem(tree, JSON.stringify(tree));
+}
+
+//saveToLocalStorage([3, 1, 2])
--- public/js/user_manual/tree.js
+++ public/js/user_manual/tree.js
@@ -1,21 +1,29 @@
 let curRootLi = -1;
-const tree = []
      
 function appendItem(){
+    const text = prompt("prompt");
+    if (text === undefined || text === null || text === ""){
+        return;
+    }
     curRootLi += 1;
     tree[curRootLi] = {
-        title: "El título " + curRootLi,
+        title: text,
         children: []
     }
     $("#root_ul").append(`<li>
-        <span onclick="editItem([${curRootLi}], 'El título ${curRootLi}');">El título ${curRootLi}</span>
-        <button class="liButtons" onclick="editLi(${curRootLi});">Editar título</button>
+        <span onclick="editItem([${curRootLi}]);">${text}</span>
+        <button class="liButtons" onclick="editLi([${curRootLi}]);">Editar título</button>
         <button class="liButtons" onclick="appendSubItem([${curRootLi}]);">Agregar subitem</button>
         <ul id="sub_ul_${curRootLi}"></ul>
     </li>`);
 }
 
 function appendSubItem(path){
+    const text = prompt("prompt");
+    if (text === undefined || text === null || text === ""){
+        return;
+    }
+
     let o = tree;
     let sub_ul_id_path = "";
     for (let i = 0; i < path.length - 1; i++) {
@@ -23,20 +31,20 @@ function appendSubItem(path){
         o = o[n].children;
         sub_ul_id_path += "_" + n;
     }
-    const closerParentIdx = path[path.length - 1]
-    curSubLiIdx = o[closerParentIdx].children.length
+    const closerParentIdx = path[path.length - 1];
+    const curSubLiIdx = o[closerParentIdx].children.length;
 
     sub_ul_id_path += "_" + closerParentIdx;
 
     o[closerParentIdx].children.push({
-        title: "El subtítulo " + sub_ul_id_path + " " + curSubLiIdx,
+        title: text,
         children: []
     })
     const nextPath = path.concat(curSubLiIdx);
 
     $("#sub_ul" + sub_ul_id_path).append(`<li>
-        <span>El sub título ${sub_ul_id_path} ${curSubLiIdx}</span>
-        <button class="liButtons" onclick="editLi();">Editar título</button>
+        <span onclick="editItem([${path}]);">${text}</span>
+        <button class="liButtons" onclick="editLi([${path}]);">Editar título</button>
         <button class="liButtons" onclick="appendSubItem([${nextPath}]);">Agregar subitem</button>
         <ul id="sub_ul${sub_ul_id_path}_${curSubLiIdx}"></ul>
     </li>`);
