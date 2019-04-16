b873001a243bce71957277454f1c98858c6166ec
aeusse <aeusse@grupodyd.com>
2019-04-15 22:41:42 -0500

    Tree deep access

--- public/js/user_manual.js
+++ public/js/user_manual.js
@@ -1,70 +1,44 @@
 let curRootLi = -1;
-let curSubLi = -1;
-
-/*const tree = [
-    {
-        title: "El título 0",
-        children: [
-            {
-                title: "El título 0 0",
-                children: []
-            },
-            {
-                title: "El título 0 1",
-                children: []
-            }
-        ]
-    },
-    {
-        title: "El título 1",
-        children: [
-            {
-                title: "El título 1 0",
-                children: []
-            },
-            {
-                title: "El título 1 1",
-                children: []
-            }
-        ]
-    }
-]; */
 const tree = []
      
 function appendItem(){
-
-
     curRootLi += 1;
     tree[curRootLi] = {
         title: "El título " + curRootLi,
         children: []
     }
-
-
     $("#root_ul").append(`<li>
         <span>El título ${curRootLi}</span>
         <button class="liButtons" onclick="editLi(${curRootLi});">Editar título</button>
-        <button class="liButtons" onclick="appendSubItem(${curRootLi});">Agregar sub-item</button>
-        <ul id="root_li_${curRootLi}"></ul>
+        <button class="liButtons" onclick="appendSubItem([${curRootLi}]);">Agregar subitem</button>
+        <ul id="sub_ul_${curRootLi}"></ul>
     </li>`);
-        
-
-
-    console.log(tree)
 }
 
-function appendSubItem(itemIdx){
-    curSubLi = tree[itemIdx].children.length;
-    tree[itemIdx].children.push({
-        title: "El título " + itemIdx + " " + curSubLi,
+function appendSubItem(path){
+    let o = tree;
+    let sub_ul_id_path = "";
+    for (let i = 0; i < path.length - 1; i++) {
+        let n = path[i];
+        o = o[n].children;
+        sub_ul_id_path += "_" + n;
+    }
+    const closerParentIdx = path[path.length - 1]
+    curSubLiIdx = o[closerParentIdx].children.length
+
+    sub_ul_id_path += "_" + closerParentIdx;
+
+    o[closerParentIdx].children.push({
+        title: "El subtítulo " + sub_ul_id_path + " " + curSubLiIdx,
         children: []
     })
+    const nextPath = path.concat(curSubLiIdx);
 
-
-    $("#root_li_" + itemIdx).append(`<li>
-        <span>El sub título ${itemIdx} ${curSubLi}</span>
-        <button class="liButtons" onclick="editLi(${curSubLi});">Editar título</button>
-        <button class="liButtons" onclick="appendSubItem(${itemIdx}, ${curSubLi});">Agregar sub-item</button>
-        <ul id="root_li_${curSubLi}"></ul>
+    $("#sub_ul" + sub_ul_id_path).append(`<li>
+        <span>El sub título ${sub_ul_id_path} ${curSubLiIdx}</span>
+        <button class="liButtons" onclick="editLi();">Editar título</button>
+        <button class="liButtons" onclick="appendSubItem([${nextPath}]);">Agregar subitem</button>
+        <ul id="sub_ul${sub_ul_id_path}_${curSubLiIdx}"></ul>
     </li>`);
+
 }
