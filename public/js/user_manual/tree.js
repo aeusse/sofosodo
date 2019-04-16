c2bef255ce2d7fea03340a57e17a5c94f7dc4e62
aeusse <aeusse@grupodyd.com>
2019-04-16 17:27:01 -0500

    Creamos funcs endpoint

--- public/js/user_manual/tree.js
+++ public/js/user_manual/tree.js
@@ -1,7 +1,13 @@
+
+// Firebase
+const firestore = firebase.firestore();
+firebase.functions()._url = function (name) {
+    return `/funcs/${name}`;
+}
+
 let curRootLi = -1;
-     
 function appendItem(){
-    const text = prompt("prompt");
+    const text = prompt("Por favor escriba el título");
     if (text === undefined || text === null || text === ""){
         return;
     }
@@ -11,42 +17,61 @@ function appendItem(){
         children: []
     }
     $("#root_ul").append(`<li>
-        <span onclick="editItem([${curRootLi}]);">${text}</span>
+        <span id="li_title_${curRootLi}" onclick="editItem([${curRootLi}]);">${text}</span>
         <button class="liButtons" onclick="editLi([${curRootLi}]);">Editar título</button>
         <button class="liButtons" onclick="appendSubItem([${curRootLi}]);">Agregar subitem</button>
         <ul id="sub_ul_${curRootLi}"></ul>
     </li>`);
 }
 
-function appendSubItem(path){
-    const text = prompt("prompt");
+function appendSubItem(parentPath){
+    const text = prompt("Por favor escriba el título");
     if (text === undefined || text === null || text === ""){
         return;
     }
 
     let o = tree;
-    let sub_ul_id_path = "";
-    for (let i = 0; i < path.length - 1; i++) {
-        let n = path[i];
+    let parentSubUlIdPath = "";
+    for (let i = 0; i < parentPath.length - 1; i++) {
+        let n = parentPath[i];
         o = o[n].children;
-        sub_ul_id_path += "_" + n;
+        parentSubUlIdPath += "_" + n;
     }
-    const closerParentIdx = path[path.length - 1];
+    const closerParentIdx = parentPath[parentPath.length - 1];
     const curSubLiIdx = o[closerParentIdx].children.length;
-
-    sub_ul_id_path += "_" + closerParentIdx;
+    parentSubUlIdPath += "_" + closerParentIdx;
 
     o[closerParentIdx].children.push({
         title: text,
         children: []
     })
-    const nextPath = path.concat(curSubLiIdx);
+    const nextPath = parentPath.concat(curSubLiIdx);
+    const nextSubUlIdPath = parentSubUlIdPath + "_" + curSubLiIdx;
 
-    $("#sub_ul" + sub_ul_id_path).append(`<li>
-        <span onclick="editItem([${path}]);">${text}</span>
-        <button class="liButtons" onclick="editLi([${path}]);">Editar título</button>
+    $("#sub_ul" + parentSubUlIdPath).append(`<li>
+        <span id="li_title${nextSubUlIdPath}" onclick="editItem([${nextPath}]);">${text}</span>
+        <button class="liButtons" onclick="editLi([${nextPath}]);">Editar título</button>
         <button class="liButtons" onclick="appendSubItem([${nextPath}]);">Agregar subitem</button>
-        <ul id="sub_ul${sub_ul_id_path}_${curSubLiIdx}"></ul>
+        <ul id="sub_ul${nextSubUlIdPath}"></ul>
     </li>`);
 
+}
+
+
+async function save(){
+    $("button").attr("disabled", true);
+    const save = firebase.functions().httpsCallable('save_user_manual');
+    try {
+        const result = await save({initial_tree: {}, tree: tree});
+        if (result.data.success === true){
+            console.log("Guardado!!!");
+        }else{
+            alert(result.data.msg);
+        }
+        $("button").attr("disabled", false);
+    } catch (error) {
+        console.log(error);
+        alert("No se pudo guardar");
+        $("button").attr("disabled", false);
+    }
 }
