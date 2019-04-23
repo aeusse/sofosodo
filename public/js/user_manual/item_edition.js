4d06e4af7e6c52726caac15d68b60c833616ac8a
aeusse <aeusse@grupodyd.com>
2019-04-23 16:52:58 -0500

    final_user_viewer, preview

--- public/js/user_manual/item_edition.js
+++ public/js/user_manual/item_edition.js
@@ -1,29 +1,42 @@
 const storageService = firebase.storage();
 const storageRef = storageService.ref();
 
-function editItem(path) {
-    //console.log(path)
+const initialFields = {};
+let modifiedData = {};
 
+function editItem(path) {
+    $('#final_user_viewer').empty();
+    $('.contents').remove();
+
+    let fullPathTitleText = "";
     let o = tree;
     for (let i = 0; i < path.length - 1; i++) {
         let n = path[i];
+        fullPathTitleText += o[n].title + "&nbsp;&nbsp;&nbsp;->&nbsp;&nbsp;&nbsp;";
         o = o[n].children;
     }
     const closerParentIdx = path[path.length - 1];
     const targetObject = o[closerParentIdx];
 
+    for (let idx in targetObject.data){
+        const field = targetObject.data[idx];
+        initialFields[idx] = field;
+        if (field.type === "text"){
+            addParagraph(parseInt(idx));
+        }else{
+            addImage(parseInt(idx));
+        }
+    }
     $("#tree_container").hide();
     $("#item_edition_title").show();
     $("#item_edition_body").show();
+    $("#item_edition_title").html(fullPathTitleText + "<b>" + targetObject.title + "</b>");
+    $('#item_edition_title').off('click');
+    $("#item_edition_title").click( function(){
+        saveToLocalStorage(path);
+    })
 
-    $("#item_edition_title").html(targetObject.title);
-
-    console.log(targetObject.data)
 }
-
-//editItem([1, 0, 1], "El título _1 0 1");
-
-
 
 function handleFileUploadSubmit(e) {
     const targetIdx = $(e.target).data().idx;
@@ -42,12 +55,21 @@ function handleFileUploadSubmit(e) {
         $(`#content_${targetIdx} div.uploadedImageDivs`).empty();
         $(`#content_${targetIdx} div.uploadedImageDivs`).prepend(`<img src="${uploadedImageUrl}" />`);
         $(`#content_${targetIdx} div.uploadedImageDivs`).data("img_url", uploadedImageUrl);
+        refreshEnteredData();
     });
 }
 
+function refreshBlurListeners(){
+    console.log("refreshBlurListeners")
+    $(".blur_trigger").unbind("blur");
+    $(".blur_trigger").blur(function() {
+        refreshEnteredData();
+    });
+    refreshEnteredData();
+}
 function addParagraph(idx) {
-    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final"><label class="where_to_show_checks"> Usuario final</label><br>
-      <textarea id="textarea_${idx}"></textarea>
+    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final" onclick="refreshEnteredData();"><label class="where_to_show_checks"> Usuario final</label><br>
+      <textarea id="textarea_${idx}" class="blur_trigger"></textarea>
       <br>
       <br>
       <button class="add_text_or_image_buttons" onclick="addParagraph(${idx + 1});">Texto</button>
@@ -60,9 +82,16 @@ function addParagraph(idx) {
         $(`#content_${idx}`).html(innerHtml);
         $(`#content_${idx}`).data("type", "text");
     }
+    if (initialFields[idx] && initialFields[idx].type === "text"){
+        $(`#textarea_${idx}`).val(initialFields[idx].text)
+        if (initialFields[idx].finalUser === true){
+            $(`#where_to_show_checkbox_${idx}`).prop('checked', true);
+        }
+    }
+    refreshBlurListeners();
 }
 function addImage(idx) {
-    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final"><label class="where_to_show_checks"> Usuario final</label><br>
+    const innerHtml = `<input id="where_to_show_checkbox_${idx}" type="checkbox" name="where_to_show" value="final" onclick="refreshEnteredData();"><label class="where_to_show_checks"> Usuario final</label><br>
       <div class="uploadedImageDivs"></div>
       <input id="file-select_${idx}" type="file" class="add_text_or_image_buttons" accept="image/*" />
       <button id="file-submit_${idx}" data-idx="${idx}" class="add_text_or_image_buttons">Subir</button>
@@ -79,28 +108,50 @@ function addImage(idx) {
         $(`#content_${idx}`).data("type", "image");
     }
     $(document).on('click', `#file-submit_${idx}`, handleFileUploadSubmit);
+    if (initialFields[idx] && initialFields[idx].type === "image"){
+        $(`#content_${idx} div.uploadedImageDivs`).empty();
+        $(`#content_${idx} div.uploadedImageDivs`).prepend(`<img src="${initialFields[idx].url}" />`);
+        $(`#content_${idx} div.uploadedImageDivs`).data("img_url", initialFields[idx].url);
+        if (initialFields[idx].finalUser === true){
+            $(`#where_to_show_checkbox_${idx}`).prop('checked', true);
+        }
+    }
+    refreshEnteredData();
+}
+
+function refreshEnteredData(){
+    console.log("refreshEnteredData")
+    let finalViewHtml = "";
+    const elements = $(".contents");
+    modifiedData = {};
+    for (let i of elements){
+        const dataIdx = $(i).data().idx;
+        modifiedData[dataIdx] = {type: $(i).data().type}
+        if($(`#where_to_show_checkbox_${dataIdx}`).is(':checked')) {
+            modifiedData[dataIdx].finalUser = true;
+        }else{
+            modifiedData[dataIdx].finalUser = false;
+        }
+        if (modifiedData[dataIdx].type === "image"){
+            modifiedData[dataIdx].url = $(`#content_${dataIdx} div.uploadedImageDivs`).data().img_url
+            if (modifiedData[dataIdx].url === undefined){
+                modifiedData[dataIdx].url = "";
+            }
+            if (modifiedData[dataIdx].finalUser){
+                finalViewHtml += `<div class="uploadedImageDivs"><img src="${modifiedData[dataIdx].url}" /></div><br>`;
+            }
+        }else{
+            modifiedData[dataIdx].text = $(`#textarea_${dataIdx}`).val();
+            if (modifiedData[dataIdx].finalUser){
+                finalViewHtml += `<div class="finalViewParagraph"><p>${modifiedData[dataIdx].text}</p></div>`;
+            }
+        }
+    }
+    $('#final_user_viewer').html(finalViewHtml);
 }
 
 function saveToLocalStorage(path){
-    const elements = $(".contents");
-    const data = {}
-    for (let i of elements){
-        const dataIdx = $(i).data().idx;
-        data[dataIdx] = {type: $(i).data().type}
-        if (data[dataIdx].type === "image"){
-            data[dataIdx].url = $(`#content_${dataIdx} div.uploadedImageDivs`).data().img_url
-            if (data[dataIdx].url === undefined){
-                data[dataIdx].url = "";
-            }
-        }else{
-            data[dataIdx].text = $(`#textarea_${dataIdx}`).val();
-        }
-        if($(`#where_to_show_checkbox_${dataIdx}`).is(':checked')) {
-            data[dataIdx].finalUser = true;
-        }else{
-            data[dataIdx].finalUser = false;
-        }
-    }
+    refreshEnteredData();
 
     let o = tree;
     for (let i = 0; i < path.length - 1; i++) {
@@ -110,9 +161,13 @@ function saveToLocalStorage(path){
     const closerParentIdx = path[path.length - 1];
     const target = o[closerParentIdx];
 
-    target.data = data;
-    localStorage.setItem("tree", JSON.stringify(tree));
-    localStorage.setItem("need_to_save", "true");
-}
+    if ((target.data === undefined && JSON.stringify(modifiedData) !== "{}") || (target.data !== undefined && (JSON.stringify(target.data) !== JSON.stringify(modifiedData)))){
+        target.data = modifiedData;
+        localStorage.setItem("tree", JSON.stringify(tree));
+        localStorage.setItem("need_to_save", "true");
+    }
 
-//saveToLocalStorage([3, 1, 2])
+    $("#item_edition_title").hide();
+    $("#item_edition_body").hide();
+    $("#tree_container").show();
+}
