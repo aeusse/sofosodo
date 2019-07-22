27971b764be82e56d2bb4f52b87498687b0d2423
aeusse <aeusse@grupodyd.com>
2019-07-22 18:19:54 -0500

    getFullMap funcionando bien

--- functions/src/db_mapper.ts
+++ functions/src/db_mapper.ts
@@ -5,7 +5,7 @@ import * as admin from 'firebase-admin'
 export async function getFullMap() {
     try {
 
-        const qantyCredentials = require('../qanty-cert.json');
+        /*const qantyCredentials = require('../qanty-cert.json');
         const qantyAppConfig = {
             credential: admin.credential.cert(qantyCredentials),
             databaseURL: "https://qanty-dev.firebaseio.com"
@@ -13,21 +13,6 @@ export async function getFullMap() {
         const adminQanty = admin.initializeApp(qantyAppConfig, "qanty")
         const qantyDb = adminQanty.firestore()
 
-
-
-
-        /*const companyData = (await qantyDb.collection("/companies").doc("57b2ORKV0Rw1u9Glnowh").get()).data()
-        console.log(companyData)
-        const companyCollections = (await qantyDb.collection("/companies").doc("57b2ORKV0Rw1u9Glnowh").getCollections())
-        for (const collection in companyCollections){
-            const algo = (await companyCollections[collection].get()).docs
-            for (const i in algo){
-                console.log(algo[i].data())
-            }
-        }*/
-        
-
-        
         const ignoredPaths = [
             "branches",
             "companyMainMaps",
@@ -46,8 +31,8 @@ export async function getFullMap() {
         ]
         const allowedToReadAllDocs = [
             "roles",
-            "sessions",
-            "users",
+            //"sessions",
+            //"users",
             "branches",
             "lines",
             "printers",
@@ -62,11 +47,14 @@ export async function getFullMap() {
         console.log("====")
         console.log("====")
         console.log(result)
-        console.log(JSON.stringify(result))
+        const resp = JSON.stringify(result)
+        console.log(resp)*/
 
+        const resp = '{"companies":{"enabled":"boolean","token":"string","thirdPartyFrontendFolder":"string","name":"string","fcmServerKey":"string","thirdPartyStrings":{"customFields":{"tipo_de_empaque":"string","tipo_de_carne":"string","tipo_de_corte":"string"}},"deleted":"boolean"},"customerDocTypes":{"shortName":"string","key":"string","name":"string"},"ownCustomers":{"docType":"string","email":"string","name":"string","mobileToken":"string","mobileId":"string","lastName":"string","docNumber":"string"},"apps":{"enabled":"boolean","deleted":"boolean","hidden":"boolean","name":"string","fcmServerKey":"string"},"ownMobiles":{"deviceId":"string","appName":"string","pushToken":"string"},"branches":{"enabled":"boolean","hidden":"boolean","gmt":"string","customBranchId":"string","name":"string","appId":"string","deleted":"boolean","branchGroupIds":{"0":"string"},"location":{"latitude":"number","longitude":"number","name":"string","address":"string"},"lastUpdate":"number"},"roles":{"lastUpdate":"number","companyId":"string","ep":"string","name":"string","rules":{"read":{"0":"string"},"update":{"0":"string"},"create":{"0":"string"}},"deleted":"boolean","isFullAdmin":"boolean","enabled":"boolean","description":"string","hidden":"boolean","profile":"string"},"tickets":{"branchName":"string","startLineId":"string","status":"string","details":"string","name":"string","activity":{"0":{"name":"string","lineName":"string","endDate":"number","startDate":"number","lineId":"string"},"1":{"slotRef":"string","startDate":"number","userId":"string","name":"string","lineId":"string","lineName":"string","endDate":"number"}},"createdBy":{"id":"string","email":"string","name":"string"},"currentLineName":"string","currentActivity":"number","lineName":"string","issueDate":"number","lastEventDate":"number","lineBranchCustomMsg":"string","origin":"string","currentLineId":"string","startLineName":"string","branchId":"string"},"lines":{"enabled":"boolean"},"realtime":{"placement":{"slotGroup":{"id":"string","name":"string"},"slot":{"displayName":"string","id":"string","name":"string"},"branch":{"gmt":"string","id":"string","twentyfourSevenService":{},"name":"string"}},"user":{"lastName":"string","firstName":"string","id":"string"},"action":"string","issueDate":"number"},"slotGroups":{"name":"string","enabled":"boolean","deleted":"boolean","hidden":"boolean"},"orders":{"allOrders":{"0":"string"},"waitingOrders":{"0":"string"}},"slots":{"status":"string","user":{"firstName":"string","id":"number","lastName":"string"},"name":"string","deleted":"boolean","type":"string","lines":{"0":"string"},"lastUser":{"id":"string","lastName":"string","firstName":"string"},"slotGroupId":"string","displayName":"string","enabled":"boolean","hidden":"boolean"},"stats":{"inTransitTickets":"number","maxServeTime":"number","reused":"number","autodumped":"number","minServeTime":"number","maxInTransitTime":"number","served":"number","waitTime":"number","tickets":"number","maxWaitTime":"number","finished":"number","successfullyServeTime":"number","inTransitTime":"number","waitingTickets":"number","minWaitTime":"number","canceled":"number","minInTransitTime":"number","dumped":"number","successfullyServed":"number","servingTickets":"number","serveTime":"number"},"detailed":{"issueDate":"number","status":"string","details":"string","createdBy":{"email":"string","name":"string","id":"string"},"customer":{"docId":"string","docType":"string","docTypeId":"string","name":"string","lastName":"string","phoneNumbers":{"0":"number"}},"customFields":{"exito":{"carnes":{"0":{"tipo_de_corte":"string","plu":"number","cantidad":"number","tipo_de_empaque":"string","precio":"number","tipo_de_carne":"string"},"1":{"tipo_de_empaque":"string","precio":"number","tipo_de_carne":"string","tipo_de_corte":"string","plu":"number","cantidad":"number"}}}}},"sessions":{"company":{"id":"string","name":"string"},"placement":{"branch":{"id":"string","name":"string","gmt":"string"}},"userId":"string","issueDate":"number","ref":"string","companyId":"string"},"touchScreens":{"name":"string","enabled":"boolean","deleted":"boolean","hidden":"boolean"},"byHour":{"reused":"number","docId":"number","autodumped":"number","served":"number","canceled":"number","dumped":"number","successfullyServed":"number","tickets":"number","finished":"number"},"users":{"deleted":"boolean","roleId":"string","lastName":"string","hidden":"boolean","firstName":"string","docNumber":"string","branches":{"0":"string","1":"string"},"docType":"string","companyId":"string","lastLogin":"string","description":"string","enabled":"boolean","password":"string","email":"string"},"printers":{"name":"string","type":"string","deleted":"boolean","interface":"string","enabled":"boolean","hidden":"boolean"},"screens":{"name":"string","token":"string","enabled":"boolean","deleted":"boolean","hidden":"boolean"},"detailedByDay":{"json":"string"},"printerHubEvents":{"printerId":"string","details":"string","ticketIssueDate":"number","ticketName":"string","lineId":"string","lineName":"string","issueDate":"number"},"tactiles":{"enabled":"boolean"},"events":{"ticketId":"string","issueDate":"number","slotDisplayName":"string","ticketName":"string","lineId":"string"},"customers":{"docId":"string","docType":"string","docTypeId":"string"},"mobiles":{"deviceId":"string","appName":"string","pushToken":"string"},"activity":{"name":"string","endDate":"number","startDate":"number","lineId":"string"}}'
 
         return{
-            success: true
+            success: true,
+            result: resp
         }
 
     } catch (error) {
@@ -81,34 +69,42 @@ export async function getFullMap() {
 
 async function processCollections(qantyDb:any, collections:any, result:any, ignoredPaths:any, allowedToReadAllDocs:any){
     
-    
+    const ps = []   //- Recolector de promesas
+    const cs = []   //- Recolector de colecciones sincronizado con ps
     for (const c of collections){
-        console.log("path:", c.path)
+        //console.log("path:", c.path)
         if (ignoredPaths.indexOf(c.path) === -1){
             /*if (c.id !== "companies"){
                 continue
             }*/
-            result[c.id] = {}
-            const docs = (await qantyDb.collection(c.path).get()).docs
-            if (allowedToReadAllDocs.indexOf(c.id) > -1){
-                for (const doc of docs){
-                    const data = doc.data()
-                    processData(result, c, data)
-                    const subCollections = await doc.ref.getCollections()
-                    await processCollections(qantyDb, subCollections, result, ignoredPaths, allowedToReadAllDocs)
-                }
+            if (allowedToReadAllDocs.indexOf(c.id) === -1){
+                ps.push(qantyDb.collection(c.path).limit(1).get())
             }else{
-                const data = docs[0].data()
-                processData(result, c, data)
-                const subCollections = await docs[0].ref.getCollections()
-                await processCollections(qantyDb, subCollections, result, ignoredPaths, allowedToReadAllDocs)
+                ps.push(qantyDb.collection(c.path).get())
             }
-
+            cs.push(c)
         }else{
-            console.log("no señor")
+            //console.log("no señor")
         }
     }
-
+    const rs = await Promise.all(ps)
+    const ps2 = []   //- Un segundo recolector de promesas
+    for (const idx in rs){
+        const docs = rs[idx].docs
+        result[cs[idx].id] = {}
+        console.log("path:", cs[idx].path)
+        for (const doc of docs){
+            try {
+                const data = doc.data()
+                processData(result, cs[idx], data)
+                const subCollections = await doc.ref.getCollections()
+                ps2.push(processCollections(qantyDb, subCollections, result, ignoredPaths, allowedToReadAllDocs))
+            } catch (error) {
+                console.log("Aquí muere esta flor")
+            }
+        }
+    }
+    await Promise.all(ps2)
     return result
 }
 
