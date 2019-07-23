7f724b1c4adb283e43adeaa6f78c1b5906a775df
aeusse <aeusse@grupodyd.com>
2019-07-23 17:32:01 -0500

    renderjson

--- public/assets/js/db_mapper/get.js
+++ public/assets/js/db_mapper/get.js
@@ -1,19 +1,24 @@
 
-// Firebase
+//- Firebase
 const firestore = firebase.firestore();
 firebase.functions()._url = function (name) {
     return `/funcs/${name}`;
 }
 
-async function getFullDbMap(){
+//- Renderjson confs
+renderjson.set_show_to_level("all");
+renderjson.set_sort_objects(true);
+
+async function getFullDbMap() {
     try {
         $("button").attr("disabled", true);
         const fullDbMapCall = firebase.functions().httpsCallable('get_full_db_map');
         const result = await fullDbMapCall();
-        if (result.data.success === true){
+        if (result.data.success === true) {
             console.log("El resultado ===========")
             console.log(result.data)
-        }else{
+            $("#walker_result").html(renderjson(result.data.json));
+        } else {
             alert(result.data.msg);
         }
         $("button").attr("disabled", false);
